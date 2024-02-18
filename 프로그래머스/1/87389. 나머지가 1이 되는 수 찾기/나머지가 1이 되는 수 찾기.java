e80418f7780c4fee2486dcbf77f2562411b57eee
Caramel <102342958+Caramel1004@users.noreply.github.com>
2024-02-19 02:50:06 +0900

    나머지가 1이 되는 수 찾기.java

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -1,4 +1,5 @@
 class Solution {
+    /** 내 풀이 */
     public int solution(int n) {
         int answer = 0;
 
@@ -22,5 +23,19 @@ class Solution {
             }
         }
         return quotient;
+    }
+
+    /** 다른 사람 풀이 */
+    public int solution2(int n) {
+        if(n % 2 != 0)
+            return 2;
+        else {
+            for(int i = 3; i <= Math.sqrt(n - 1); i += 2) { // 이 부분은 수학적으로 생각 해 볼 필요가 있을거 같다.
+                if((n - 1) % i == 0) 
+                    return i;
+            }
+        }
+
+        return n - 1;
     }
 }
