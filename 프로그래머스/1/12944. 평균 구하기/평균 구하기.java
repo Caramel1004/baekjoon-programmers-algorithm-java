b9fdcf57fb1493e2045ed01e8a559ee5c544b580
Caramel <102342958+Caramel1004@users.noreply.github.com>
2024-02-19 01:23:06 +0900

    평균 구하기.java

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,4 +1,5 @@
 class Solution {
+    /** 방법(내 풀이) 1. 반복문으로 처리 */
     public double solution(int[] arr) {
         int arrLength = arr.length;
         int sum = 0;
@@ -10,5 +11,9 @@ class Solution {
         // 평균
         return (double) sum / (double) arrLength;
     }
-    
+
+    /** 방법 2. 스트림으로 변환하여 평균내기 */
+    public double solution2(int[] arr) {
+      return (double) Arrays.stream(arr).average().orElse(0);
+    }
 }
