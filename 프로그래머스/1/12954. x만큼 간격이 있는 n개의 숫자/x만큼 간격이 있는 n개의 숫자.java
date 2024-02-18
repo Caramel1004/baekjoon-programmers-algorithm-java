2b5e487463b613f56c16ec237c49bb7bea0caec5
Caramel <102342958+Caramel1004@users.noreply.github.com>
2024-02-18 23:47:14 +0900

    x만큼 간격이 있는 n개의 숫자.java

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -2,6 +2,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
+
+    /** 방법(내 풀이) 1. ArrayList에 요소 추가해서 반복문으로 처리 */
     public List<Long> solution(int x, int n) {
         // 1. element타입이 long인 ArrayList 선언
         List<Long> answer = new ArrayList<Long>();
@@ -12,5 +14,14 @@ class Solution {
         }
         
         return answer;
+    }
+
+    /** 방법 2. stream으로 처리 */
+    public long[] solution2(int x, int n) {
+        return LongStream.iterate(x, i->i+x).limit(n).toArray();
+    }
+    
+    public long[] solution2_1(int x, int n) {
+        return LongStream.rangeClosed(1, n).map(v -> v *= x).toArray();
     }
 }
