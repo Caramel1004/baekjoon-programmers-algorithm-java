ab28a121c123210a2f156f1d942af1c25a4db131
Caramel <102342958+Caramel1004@users.noreply.github.com>
2024-02-20 13:43:05 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 class Solution {
     /** 내 풀이 */
     public int solution(int n) {
@@ -12,9 +14,9 @@ class Solution {
     }
     
     /** 다른 풀이 stream으로 처리 */
-    // public int solution2(int n) {
-    //     return IntStream.rangeClosed(0, n)
-    //         .filter(e -> e % 2 == 0)
-    //         .sum();
-    // }
+    public int solution2(int n) {
+        return IntStream.rangeClosed(0, n)
+            .filter(e -> e % 2 == 0)
+            .sum();
+    }
 }
