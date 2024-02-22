2d983938748341e4543864a7ee1fdabd0690d67a
Caramel <102342958+Caramel1004@users.noreply.github.com>
2024-02-22 11:04:33 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,8 +1,19 @@
 import java.util.Arrays;
 
 class Solution {
+    /** 내 풀이1 */
     public static int[] solution(int[] numbers, int num1, int num2) {
         // 인덱스 num1 부터 인덱스 num2까지 배열 카피
         return Arrays.copyOfRange(numbers, num1, num2 + 1);
+    }
+
+    /** 내 풀이2 */
+    public int[] solution2(int[] numbers, int num1, int num2) {
+        return IntStream.rangeClosed(num1, num2).map(i -> numbers[i]).toArray();
+    }
+
+    /** 내 풀이3 */
+    public int[] solution3(int[] numbers, int num1, int num2) {
+        return Arrays.stream(numbers, num1, num2+1).toArray();
     }
 }
