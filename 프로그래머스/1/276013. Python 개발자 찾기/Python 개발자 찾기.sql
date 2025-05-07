66eaedc2c6da1d46e1a0d5ba396a5055267beaaf
Caramel <102342958+Caramel1004@users.noreply.github.com>
2025-05-07 23:50:26 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -0,0 +1,11 @@
+select
+    id,
+    email,
+    first_name,
+    last_name
+from
+    DEVELOPER_INFOS
+where
+    skill_1 = 'Python' or skill_2 = 'Python' or skill_3 = 'Python'
+order by
+    id ASC;
