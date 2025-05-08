5b5921b82a9a3b9b3ea3a2bc6a02b2784f278015
Caramel <102342958+Caramel1004@users.noreply.github.com>
2025-05-08 17:02:40 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,14 +1,16 @@
-# SKILLCODES: 프로그래밍 언어에 대한 정보를 담은 테이블
-# DEVELOPERS: 프로그래밍 스킬 정보를 담은 테이블
-
 SELECT
     id,
     email,
     first_name,
     last_name
 FROM
-    DEVELOPERS
+    DEVELOPERS dev
 WHERE
-    SKILL_CODE & (1 << 8) | SKILL_CODE & (1 << 10)
+    dev.skill_code & 
+        (
+            SELECT BIT_OR(code)
+            FROM SKILLCODES
+            WHERE name IN ('Python', 'C#')
+        ) > 0
 ORDER BY
     id ASC;
