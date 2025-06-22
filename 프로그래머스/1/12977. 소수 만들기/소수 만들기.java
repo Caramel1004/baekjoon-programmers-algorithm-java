cb55118761e1805ee415efe91e66ec81443178a4
Caramel <102342958+Caramel1004@users.noreply.github.com>
2025-06-22 20:14:07 +0900

    [level 1] Title: 소수 만들기, Time: 4.97 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -52,7 +52,6 @@ class Solution {
     }
 
     private static boolean isPrime(int num) {
-        boolean isTrue = true;
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
