fce205fede884fc21450f929f1e29a6edc336a11
Caramel <102342958+Caramel1004@users.noreply.github.com>
2025-08-16 00:38:20 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 51.12 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=java) 
 
 ### 성능 요약
 
-메모리: 39.6 MB, 시간: 20.96 ms
+메모리: 108 MB, 시간: 51.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 15일 23:48:49
+2025년 08월 16일 00:38:16
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String s) {
+        return firstSolution(s);
+        // return secondSolution(s);
+    }
+    
+    private static int[] firstSolution(String str) {
+        int removedZerosCount = 0;
+        int trialCount = 0;
+        String keyword = str;
+        while (!keyword.equals("1")) {
+            int onesCount = 0;
+            String[] keywords = keyword.split("");
+            for (String s : keywords) {
+                if (s.equals("0")) {
+                    removedZerosCount++;
+                }
+                if (s.equals("1")) {
+                    onesCount++;
+                }
+            }
+            keyword = Integer.toBinaryString(onesCount);
+            trialCount++;
+        }
+        return new int[]{trialCount, removedZerosCount};
+    }
+
+    private static int[] secondSolution(String str) {
+        int removedZerosCount = 0;
+        int trialCount = 0;
+        String keyword = str;
+        while (!keyword.equals("1")) {
+            String[] keywords = keyword.split("");
+            int onesCount = (int) Arrays.stream(keywords)
+                    .filter(s -> s.equals("1"))
+                    .count();
+            removedZerosCount += keywords.length - onesCount;
+            keyword = Integer.toBinaryString(onesCount);
+            trialCount++;
+        }
+        return new int[]{trialCount, removedZerosCount};
+    }
+}
