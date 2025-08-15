55f6082b3eced81ee44124d1ff18af327a3273aa
Caramel <102342958+Caramel1004@users.noreply.github.com>
2025-08-15 14:34:36 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int n) {
-        return search(n);
+        return secondSolution(n);
     }
     
     /**
@@ -19,7 +19,7 @@ class Solution {
      */
     
     // 시간 초과
-    private static int search(int num) {
+    private static int firstSolution(int num) {
         int bitOnesCount = Integer.bitCount(num);
         // 1. 2의 거듭 제곱 수는 다음 거듭 제곱 수로 처리
         if (bitOnesCount == 1) {
@@ -73,10 +73,71 @@ class Solution {
                 }
             }
         }
-        int sum = 0;
-        for(int pow : nextBitPowers) {
-            sum += pow;
+        return Arrays.stream(nextBitPowers).sum();
+    }
+    
+    // 통과
+    // return Arrays.stream(nextBitPowers).sum() => 향상된 for문으로 누적 합 코드로 변경
+    // 실행 시간 조건 있으면, 되도록 stream 변환 코드는 지양
+    private static int secondSolution(int num) {
+        int bitOnesCount = Integer.bitCount(num);
+        // 1. 2의 거듭 제곱 수는 다음 거듭 제곱 수로 처리
+        if (bitOnesCount == 1) {
+            return num << 1;
         }
-        return sum;
+        // 2. num을 2의 거듭 제곱 수들로 분해
+        int[] bitPowers = new int[bitOnesCount];
+        int operand = num;
+        for (int i = 0; i < bitPowers.length; i++) {
+            int totalBitCount = Integer.toBinaryString(operand).length(); // 총 비트 수
+            int exponent = totalBitCount - 1;
+            int highestPowerOfTwo = 1 << exponent;
+            bitPowers[i] = highestPowerOfTwo;
+            operand %= highestPowerOfTwo;
+        }
+        /* 3. 맨 오른쪽부터 순차적으로 왼쪽 쉬프트가 가능한 비트 탐색
+         * Exam)
+         * 1100011 -> 1100101
+         * 1000111 -> 1001011
+         * 1001011 -> 1001101
+         * 1001101 -> 1001110
+         * 10010110 -> 10011001
+         * 1111000 -> 10000111
+         */
+        int[] nextBitPowers = Arrays.copyOf(bitPowers, bitPowers.length);
+        for (int idx = nextBitPowers.length - 1; idx > 0; idx--) {
+            // n번째 비트가 왼쪽으로 쉬프트가 가능한 경우
+            if (nextBitPowers[idx - 1] != nextBitPowers[idx] << 1) {
+                nextBitPowers[idx] <<= 1;
+                int remainedOnesCount = bitOnesCount - (idx + 1);
+                int currentBit = 1;
+                // 맨 오른쪽 비트부터 순차적으로 1 채우기
+                for (int i = nextBitPowers.length - 1; remainedOnesCount > 0; i--) {
+                    nextBitPowers[i] = currentBit;
+                    currentBit <<= 1;
+                    remainedOnesCount--;
+                }
+                break;
+            }
+            // 인덱스 1번째 요소도 왼쪽 쉬프트가 불가능한 경우
+            if(idx == 1) {
+                // 0번째 요소 왼쪽 쉬프트
+                nextBitPowers[idx - 1] <<= 1;
+                int remainedOnesCount = bitOnesCount - idx;
+                int currentBit = 1;
+                // 맨 오른쪽부터 순차적으로 1채우기
+                for (int i = nextBitPowers.length - 1; remainedOnesCount > 0; i--) {
+                    nextBitPowers[i] = currentBit;
+                    currentBit <<= 1;
+                    remainedOnesCount--;
+                }
+            }
+        }
+        
+        int nextBigNum = 0;
+        for(int pow : nextBitPowers) {
+            nextBigNum += pow;
+        }
+        return nextBigNum;
     }
 }
