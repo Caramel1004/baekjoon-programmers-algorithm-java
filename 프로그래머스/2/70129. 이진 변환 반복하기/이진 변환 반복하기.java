aac86185b26c4b35c980d4133aac3fed91d3a043
Caramel <102342958+Caramel1004@users.noreply.github.com>
2025-08-16 00:43:47 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.76 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(String s) {
         // return firstSolution(s);
-        return secondSolution(s);
+        // return secondSolution(s);
+        return thirdSolution(s);
     }
     
     // 메모리: 108 MB, 시간: 51.12 ms
@@ -28,6 +29,7 @@ class Solution {
         return new int[]{trialCount, removedZerosCount};
     }
 
+    // 메모리: 88.5 MB, 시간: 61.22 ms
     private static int[] secondSolution(String str) {
         int removedZerosCount = 0;
         int trialCount = 0;
@@ -38,6 +40,26 @@ class Solution {
                     .filter(s -> s.equals("1"))
                     .count();
             removedZerosCount += keywords.length - onesCount;
+            keyword = Integer.toBinaryString(onesCount);
+            trialCount++;
+        }
+        return new int[]{trialCount, removedZerosCount};
+    }
+    
+    private static int[] thirdSolution(String str) {
+        int removedZerosCount = 0;
+        int trialCount = 0;
+        String keyword = str;
+        while (!keyword.equals("1")) {
+            int onesCount = 0;
+            for (int idx = 0; idx < keyword.length(); idx++) {
+                if (keyword.charAt(idx) == '0') {
+                    removedZerosCount++;
+                }
+                if (keyword.charAt(idx) == '1') {
+                    onesCount++;
+                }
+            }
             keyword = Integer.toBinaryString(onesCount);
             trialCount++;
         }
