98f48d711496de2badb270b4bf8c2be0bf823d39
Caramel <102342958+Caramel1004@users.noreply.github.com>
2025-08-20 03:23:29 +0900

    [Silver IV] Title: 덱, Time: 164 ms, Memory: 18492 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int commandCount = Integer.parseInt(st.nextToken());
+        Deque<String> deque = new ArrayDeque<>();
+        /*
+        *   큐 방향
+        *   -----------
+        *   <- <- <- <-
+        *   -----------
+        */
+        for (int i = 0; i < commandCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            String value = st.hasMoreTokens() ? st.nextToken() : null;
+            String elem = "";
+            switch (command) {
+                case "push_front": 
+                    deque.addFirst(value);
+                    break;
+                case "push_back": 
+                    deque.offer(value);
+                    break;
+                case "pop_front":
+                    elem = deque.pollFirst();
+                    sb.append(elem == null ? -1 : elem).append("\n");
+                    break;
+                case "pop_back":
+                    elem = deque.pollLast();
+                    sb.append(elem == null ? -1 : elem).append("\n");
+                    break;
+                case "size":
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(deque.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    elem = deque.peekFirst();
+                    sb.append(elem == null ? -1 : elem).append("\n");
+                    break;
+                case "back":
+                    elem = deque.peekLast();
+                    sb.append(elem == null ? -1 : elem).append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
