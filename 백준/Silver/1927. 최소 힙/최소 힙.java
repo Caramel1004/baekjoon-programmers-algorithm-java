2bc0d0af3be63ec17f236a21b46e44f9d48d83b9
Caramel <102342958+Caramel1004@users.noreply.github.com>
2025-08-27 13:31:37 +0900

    [Silver II] Title: 최소 힙, Time: 284 ms, Memory: 27040 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int size = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < size; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num != 0) {
+                pq.offer(num);
+            } else {
+                int max = pq.peek() == null ? 0 : pq.poll();
+                sb.append(max).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
