3110d5b92710ed75acdff253e1231e097c37ebf7
Caramel <102342958+Caramel1004@users.noreply.github.com>
2025-09-01 22:19:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 72 ms, Memory: 12280 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
@@ -2,18 +2,22 @@ fun main() {
     val br = System.`in`.bufferedReader()
     val sb = StringBuilder()
 
+    val max = 10000
     val size = br.readLine().toInt()
+
+    val nums = IntArray(max + 1)
+
+    nums[1] = 1
+    nums[2] = 2
+    nums[3] = 4
+
+    for (i in 4..10000) {
+        nums[i] = nums[i - 1] + nums[i - 2] + nums[i - 3]
+    }
 
     repeat(size) {
         val num = br.readLine().toInt()
-        sb.append(tribonacci(num)).append("\n")
+        sb.append(nums[num]).append("\n")
     }
     print(sb)
-}
-
-private fun tribonacci(num: Int): Int = when (num) {
-    3 -> 4
-    2 -> 2
-    1 -> 1
-    else -> tribonacci(num - 1) + tribonacci(num - 2) + tribonacci(num - 3)
 }
